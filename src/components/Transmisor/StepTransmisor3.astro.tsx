0992b611fc83683aece10d900cbc79db0b6b34d1
Francisco Perez <franutn23@gmail.com>
2024-10-02 23:13:47 -0300

    add :

--- src/components/Transmisor/StepTransmisor3.astro.tsx
+++ src/components/Transmisor/StepTransmisor3.astro.tsx
@@ -5,7 +5,7 @@ function StepTransmisor2({ secuence, setSecuence }: { secuence: string, setSecue
   return (
     <>
       <p className="text-gray-300 mb-3">
-        Procedemos a agregar los bits de paridad de la siguiente forma
+        Procedemos a agregar los bits de paridad de la siguiente forma: 
       </p>
     </>
   )
